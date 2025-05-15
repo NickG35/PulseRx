48aad36e1b6238658340ab1bdb22e1c24e91e48c
NickG35 <ngeorge356@gmail.com>
2025-05-15 07:00:03 -0400

    implemented custom account model

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,10 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
-
 # Create your models here.
+
+class CustomAccount(AbstractUser):
+    ROLE_CHOICES = (
+        ('pharmacy', 'Pharmacy'),
+        ('patient', 'Patient'),
+    )
+    role = models.CharField(max_length=10, choices=ROLE_CHOICES)
