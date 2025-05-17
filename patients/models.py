47e24fef76f7cd37fcefa2fe269a4aebf39b6c9a
NickG35 <ngeorge356@gmail.com>
2025-05-17 06:14:20 -0400

    initialized patient profile model

--- patients/models.py
+++ patients/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from accounts.models import CustomAccount
 
-# Create your models here.
+class PatientProfile(models.Model):
+    user = models.OneToOneField(CustomAccount, on_delete=models.CASCADE)
+    dob = models.DateField()
+    gender = models.CharField(max_length=10, choices=[('M', 'Male'), ('F', 'Female')])
+    phone_number = models.CharField(max_length=20, blank=True)
+    current_medications = models.TextField(blank=True)
