c9d08544c511ea14338f8afcb49ba705cdff8e97
NickG35 <ngeorge356@gmail.com>
2025-05-18 14:34:11 -0400

    adjusted patient profile and implemented med reminder

--- patients/models.py
+++ patients/models.py
@@ -1,9 +1,16 @@
 from django.db import models
 from accounts.models import CustomAccount
+from pharmacy.models import Drug
 
 class PatientProfile(models.Model):
     user = models.OneToOneField(CustomAccount, on_delete=models.CASCADE)
     dob = models.DateField()
     gender = models.CharField(max_length=10, choices=[('M', 'Male'), ('F', 'Female')])
     phone_number = models.CharField(max_length=20, blank=True)
-    current_medications = models.TextField(blank=True)
+
+class MedicationReminder(models.Model):
+    patient = models.ForeignKey(PatientProfile, on_delete=models.CASCADE)
+    medicine = models.ForeignKey(Drug, on_delete=models.PROTECT)
+    frequency = models.CharField(max_length=100)
+    time = models.TimeField()
+
