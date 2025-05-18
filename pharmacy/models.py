2f415176f58b4eac80c09851e3b58bd24d580107
NickG35 <ngeorge356@gmail.com>
2025-05-18 14:34:55 -0400

    added drug and prescription models to pharmacy app

--- pharmacy/models.py
+++ pharmacy/models.py
@@ -5,3 +5,20 @@ class PharmacyProfile(models.Model):
     user = models.OneToOneField(CustomAccount, on_delete=models.CASCADE)
     pharmacy_name = models.CharField(max_length=255)
     address = models.TextField()
+
+class Drug(models.Model):
+    name = models.CharField(max_length=255)
+    brand = models.CharField(max_length=255, blank=True)
+    description = models.TextField()
+    dosage = models.CharField(max_length=100)
+    stock = models.IntegerField(default=100)
+
+class Prescription (models.Model):
+    patient = models.ForeignKey(CustomAccount, related_name='patient', on_delete=models.SET_NULL, null=True, limit_choices_to={'role': 'patient'})
+    medicine = models.ForeignKey(Drug, related_name='drug', on_delete=models.PROTECT)
+    quantity = models.IntegerField(default=0)
+    prescribed_by = models.ForeignKey(CustomAccount, related_name='pharmacist', on_delete=models.SET_NULL, null=True, limit_choices_to={'role': 'pharmacy'})
+    prescribed_on = models.DateTimeField(auto_now_add=True)
+    expiration_date = models.DateField()
+
+
