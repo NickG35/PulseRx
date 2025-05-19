dc0049a5823608ed75676396fdf294fa80216d10
NickG35 <ngeorge356@gmail.com>
2025-05-19 06:08:02 -0400

    set up base user registration form with role choice field

---
+++ accounts/forms.py
@@ -0,0 +1,14 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from .models import CustomAccount
+
+class UserRegistrationForm(UserCreationForm):
+    ROLE_CHOICES = (
+        ('pharmacy', 'Pharmacy'),
+        ('patient', 'Patient'),
+    )
+    role = forms.ChoiceField(max_length=10, choices=ROLE_CHOICES)
+
+    class Meta:
+        model = CustomAccount
+        fields = ['username', 'email', 'password1', 'password2', 'role']
