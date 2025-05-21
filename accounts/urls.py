44710e6ebe7effaa00a1e905b30b4560f43a1ece
NickG35 <ngeorge356@gmail.com>
2025-05-21 14:32:25 -0400

    set login url

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.CustomLoginView.as_view(), name='login'),
+    path('login', views.CustomLoginView.as_view(), name='login'),
     path('register', views.role_picker, name='register'),
     path('register/<str:role>', views.register_role, name='register_role'),
 ]
