736fb568887555d7e115e57a7e70dba8ffb30919
NickG35 <ngeorge356@gmail.com>
2025-05-21 07:41:50 -0400

    registered pharmacy profile and prescription in admin

--- pharmacy/admin.py
+++ pharmacy/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import PharmacyProfile, Prescription
 
 # Register your models here.
+admin.site.register(PharmacyProfile)
+admin.site.register(Prescription)
