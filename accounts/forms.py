9bedcc7e8def29d8af7de896fd3a5e0e2aaa60db
NickG35 <ngeorge356@gmail.com>
2025-05-21 07:10:49 -0400

    removed display of role field

--- accounts/forms.py
+++ accounts/forms.py
@@ -3,12 +3,10 @@ from django.contrib.auth.forms import UserCreationForm
 from .models import CustomAccount
 
 class UserRegistrationForm(UserCreationForm):
-    ROLE_CHOICES = (
-        ('pharmacy', 'Pharmacy'),
-        ('patient', 'Patient'),
-    )
-    role = forms.ChoiceField(choices=ROLE_CHOICES)
-
     class Meta:
         model = CustomAccount
         fields = ['username', 'email', 'password1', 'password2', 'role']
+    
+    def __init__(self, *args, **kwargs):
+        super(UserCreationForm, self).__init__(*args, **kwargs)
+        self.fields['role'].widget = forms.HiddenInput()
