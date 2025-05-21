1a505d4aea70aadbdf936180e3ef4e9aee456b7a
NickG35 <ngeorge356@gmail.com>
2025-05-21 17:36:03 -0400

    implemented login redirect view, logout, and altered custom login view

--- accounts/views.py
+++ accounts/views.py
@@ -2,21 +2,27 @@ from django.shortcuts import render, redirect
 from .forms import UserRegistrationForm, LoginForm
 from pharmacy.forms import PharmacyProfileForm
 from patients.forms import PatientProfileForm
-from django.contrib.auth import login
+from django.contrib.auth import login, logout
 from django.contrib.auth.views import LoginView
 from django.urls import reverse_lazy
+from django.contrib.auth.decorators import login_required
+
 
 # Create your views here.
 class CustomLoginView(LoginView):
     template_name = 'login.html'
     authentication_form = LoginForm
 
-    def redirect_url(self):
+    def get_success_url(self):
         user = self.request.user
         if user.role == 'pharmacy':
             return reverse_lazy('pharmacy_home')
         elif user.role == 'patient':
             return reverse_lazy('patient_home')
+        
+def logout_view(request):
+    logout(request)
+    return redirect('login')
 
 
 def role_picker(request):
@@ -24,7 +30,14 @@ def role_picker(request):
         role=request.POST.get('role')
         return redirect('register_role', role=role)
     return render(request, 'register.html')
-        
+
+@login_required
+def role_redirect(request):
+    if request.user.role == 'pharmacy':
+        return redirect('pharmacy_home')
+    elif request.user.role == 'patient':
+        return redirect('patient_home')
+    return redirect('login') 
     
 
 def register_role(request, role):
