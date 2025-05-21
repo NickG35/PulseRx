b5d2bd47f2ad889feecb26646913e8b67c595b89
NickG35 <ngeorge356@gmail.com>
2025-05-21 17:35:26 -0400

    added logout url

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('login', views.CustomLoginView.as_view(), name='login'),
+    path('logout', views.logout_view, name='logout'),
     path('register', views.role_picker, name='register'),
     path('register/<str:role>', views.register_role, name='register_role'),
 ]
