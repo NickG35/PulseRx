73dc595363ac9914468853ffdc9ca30971f4aa75
NickG35 <ngeorge356@gmail.com>
2025-05-22 08:13:03 -0400

    added initial patient views

--- patients/views.py
+++ patients/views.py
@@ -2,3 +2,18 @@ from django.shortcuts import render
 
 def patient_home(request):
     return render(request, 'patient_home.html')
+
+def prescriptions(request):
+    return render(request, 'prescriptions.html')
+
+def my_pharmacy(request):
+    return render(request, 'my_pharmacy.html')
+
+def account(request):
+    return render(request, 'account.html')
+
+def reminders(request):
+    return render(request, 'reminders.html')
+
+def messages(request):
+    return render(request, 'messages.html')
