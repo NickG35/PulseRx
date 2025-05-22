e38ac956af58a1282cccdadb9edda2f18d4663b6
NickG35 <ngeorge356@gmail.com>
2025-05-22 08:12:40 -0400

    added more patient urls

--- patients/urls.py
+++ patients/urls.py
@@ -3,4 +3,9 @@ from . import views
 
 urlpatterns = [
     path('dashboard', views.patient_home, name='patient_home'),
+    path('prescriptions', views.prescriptions, name='prescriptions'),
+    path('my_pharmacy', views.my_pharmacy, name='my_pharmacy'),
+    path('account', views.account, name='account'),
+    path('reminders', views.reminders, name='reminders'),
+    path('messages', views.messages, name='messages'),
 ]
