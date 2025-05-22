52c3f412cf60fa7f63d4c3046ac6b337d64dc244
NickG35 <ngeorge356@gmail.com>
2025-05-22 18:27:49 -0400

    created pharmacy specific views

--- pharmacy/views.py
+++ pharmacy/views.py
@@ -3,3 +3,18 @@ from django.shortcuts import render
 # Create your views here.
 def pharmacy_home(request):
     return render(request, 'pharmacy_home.html')
+
+def create_prescriptions(request):
+    return render(request, 'create_prescriptions.html')
+
+def my_patients(request):
+    return render(request, 'my_patients.html')
+
+def account(request):
+    return render(request, 'pharmacy_account.html')
+
+def inventory(request):
+    return render(request, 'inventory.html')
+
+def pharmacy_messages(request):
+    return render(request, 'pharmacy_messages.html')
