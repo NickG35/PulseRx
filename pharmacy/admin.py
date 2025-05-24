ba9b0da0150c35fe5203e2641e3088be87cdceab
NickG35 <ngeorge356@gmail.com>
2025-05-24 07:51:57 -0400

    registered drug model on admin page

--- pharmacy/admin.py
+++ pharmacy/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import PharmacyProfile, Prescription
+from .models import PharmacyProfile, Prescription, Drug
 
 # Register your models here.
 admin.site.register(PharmacyProfile)
 admin.site.register(Prescription)
+admin.site.register(Drug)
