64bcdd9816b5500cf49c6afefe56d981c2d610f6
NickG35 <ngeorge356@gmail.com>
2025-06-02 05:58:45 -0400

    implemented summary function for concise descriptions and dosage

--- pharmacy/management/commands/import_drugs.py
+++ pharmacy/management/commands/import_drugs.py
@@ -1,6 +1,22 @@
 import requests
 from django.core.management.base import BaseCommand
 from pharmacy.models import Drug
+from sumy.parsers.plaintext import PlaintextParser
+from sumy.nlp.tokenizers import Tokenizer
+from sumy.summarizers.lex_rank import LexRankSummarizer
+
+import nltk
+
+try:
+    nltk.data.find('tokenizers/punkt')
+except LookupError:
+    nltk.download('punkt')
+
+def summarize_text(text, sentences_count=3):
+    parser = PlaintextParser.from_string(text, Tokenizer("english"))
+    summarizer = LexRankSummarizer()
+    summary = summarizer(parser.document, sentences_count)
+    return " ".join(str(sentence) for sentence in summary)
 
 POPULAR_DRUGS = [
     "Acetaminophen", "Hydrocodone", "Albuterol", "Alendronate", "Allopurinol", "Alprazolam", "Amoxicillin", "Amoxicillin/Clavulanate", "Amphetamine/Dextroamphetamine", "Amlodipine", "Aripiprazole", "Atenolol", "Atorvastatin", "Azithromycin", "Baclofen", "Benazepril", "Bisoprolol", "Bupropion", "Buspirone", "Butalbital/Acetaminophen/Caffeine", "Buprenorphine/Naloxone", "Captopril", "Carvedilol", "Cefdinir", "Cefuroxime", "Celecoxib", "Cetirizine", "Ciprofloxacin", "Citalopram", "Clindamycin", "Clonazepam", "Clopidogrel", "Cyclobenzaprine", "Dapagliflozin", "Desvenlafaxine", "Diazepam", "Diclofenac", "Dicyclomine", "Digoxin", "Diltiazem", "Diphenhydramine", "Divalproex", "Doxycycline", "Dulaglutide", "Enalapril", "Escitalopram", "Esomeprazole", "Estradiol", "Eszopiclone", "Fentanyl", "Fexofenadine", "Finasteride", "Fluconazole", "Fluoxetine", "Fluticasone", "Fluticasone/Salmeterol", "Gabapentin", "Gemfibrozil", "Glimepiride", "Glipizide", "Hydrochlorothiazide", "Hydrocodone/Acetaminophen", "Hydroxychloroquine", "Ibuprofen", "Insulin Aspart", "Insulin Detemir", "Insulin Glargine", "Insulin Lispro", "Ipratropium/Albuterol", "Isosorbide Mononitrate", "Lamotrigine", "Lansoprazole", "Levetiracetam", "Levothyroxine", "Lisinopril", "Lorazepam", "Losartan", "Lovastatin", "Lurasidone", "Meloxicam", "Memantine", "Metformin", "Methadone", "Methocarbamol", "Methylphenidate", "Metoprolol", "Metronidazole", "Mirtazapine", "Montelukast", "Naproxen", "Nitroglycerin", "Nortriptyline", "Omeprazole", "Ondansetron", "Oxycodone", "Oxycodone/Acetaminophen", "Pantoprazole", "Paroxetine", "Penicillin", "Phenytoin", "Pioglitazone", "Prednisone", "Pregabalin", "Promethazine", "Propranolol", "Quetiapine", "Rabeprazole", "Ranitidine", "Risperidone", "Rosuvastatin", "Sertraline", "Sildenafil", "Simvastatin", "Sitagliptin", "Spironolactone", "Tamsulosin", "Temazepam", "Terazosin", "Tizanidine", "Topiramate", "Tramadol", "Trazodone", "Triamterene/Hydrochlorothiazide", "Valsartan", "Venlafaxine", "Verapamil", "Warfarin", "Zaleplon", "Zolpidem", "Zolmitriptan",
@@ -33,13 +49,15 @@ class Command(BaseCommand):
                 route = openfda.get('route', [None])[0]
                 description = data.get("description", [""])[0]
                 dosage = data.get("dosage_and_administration", [""])[0]
+                summary_description = summarize_text(description, sentences_count=3)
+                summary_dosage = summarize_text(dosage, sentences_count=3)
 
                 Drug.objects.create(
                     name=name,
                     brand=brand,
                     route=route,
-                    description=description, 
-                    dosage=dosage
+                    description=summary_description, 
+                    dosage=summary_dosage
                 )
                 self.stdout.write(self.style.SUCCESS(f"Added drug: {name}"))
             except requests.RequestException as e:
