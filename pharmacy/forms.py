71ac05e9aba72f29a0262cacd988a0abe6de1a8b
NickG35 <ngeorge356@gmail.com>
2025-06-04 08:11:19 -0400

    form includes every field aside from join code

--- pharmacy/forms.py
+++ pharmacy/forms.py
@@ -4,4 +4,4 @@ from .models import PharmacyProfile
 class PharmacyProfileForm(forms.ModelForm):
     class Meta:
         model = PharmacyProfile
-        fields = ['pharmacy_name', 'address']
+        exclude = ['join_code']
