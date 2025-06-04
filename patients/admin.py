8a2c47c9c59a0cb23ec19deaa9d0580adafc17bd
NickG35 <ngeorge356@gmail.com>
2025-06-04 07:17:52 -0400

    registered patientpharmacy model to admin

--- patients/admin.py
+++ patients/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import PatientProfile
+from .models import PatientProfile, PatientPharmacy
 
 # Register your models here.
 admin.site.register(PatientProfile)
+admin.site.register(PatientPharmacy)
