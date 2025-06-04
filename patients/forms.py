d56530b48ceaed56697630b33837889d402fdb90
NickG35 <ngeorge356@gmail.com>
2025-06-04 07:20:40 -0400

    created a pharmacy form for patients to select from

--- patients/forms.py
+++ patients/forms.py
@@ -1,7 +1,16 @@
 from django import forms
 from .models import PatientProfile
+from pharmacy.models import PharmacyProfile
 
 class PatientProfileForm(forms.ModelForm):
     class Meta:
         model = PatientProfile
         fields = ['dob', 'gender', 'phone_number']
+
+class PharmacistForm(forms.Form):
+    pharmacy = forms.ModelChoiceField(
+        queryset=PharmacyProfile.objects.all(),
+        label="Select a Pharmacy",
+        empty_label="Choose a pharmacy",
+        required=True
+    )
