6d55041cca68ba263d72b6f0874260eb41244676
NickG35 <ngeorge356@gmail.com>
2025-06-04 07:23:42 -0400

    implemented pharmacy form for patients

--- patients/views.py
+++ patients/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import PharmacistForm
+from .models import PatientPharmacy
 
 def patient_home(request):
     return render(request, 'patient_home.html')
@@ -7,7 +9,21 @@ def prescriptions(request):
     return render(request, 'prescriptions.html')
 
 def my_pharmacy(request):
-    return render(request, 'my_pharmacy.html')
+    user = request.user
+    patient_profile, _ = PatientPharmacy.objects.get_or_create(patient=user)
+
+    if request.method == 'POST':
+        form = PharmacistForm(request.POST)
+        if form.is_valid():
+            patient_profile.pharmacy = form.cleaned_data['pharmacy']
+            patient_profile.save()
+            return redirect('my_pharmacy')
+        
+    form = PharmacistForm()
+    return render(request, 'my_pharmacy.html' , {
+        'pharmacies': patient_profile.pharmacy,
+        'form': form
+    })
 
 def account(request):
     return render(request, 'account.html')
