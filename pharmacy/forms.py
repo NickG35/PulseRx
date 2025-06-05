bb682d6d81ebdfad6b85dcdd209a740d58cf7871
NickG35 <ngeorge356@gmail.com>
2025-06-05 12:21:20 -0400

    added pharmacist profile form and excluded user fields

--- pharmacy/forms.py
+++ pharmacy/forms.py
@@ -1,7 +1,13 @@
 from django import forms
-from .models import PharmacyProfile
+from .models import PharmacyProfile, PharmacistProfile
 
 class PharmacyProfileForm(forms.ModelForm):
     class Meta:
         model = PharmacyProfile
-        exclude = ['join_code']
+        exclude = ['user','join_code']
+
+class PharmacistProfileForm(forms.ModelForm):
+    join_code = forms.CharField(max_length=6, required=True, help_text="Enter your pharmacy's join code")
+    class Meta:
+        model = PharmacistProfile
+        exclude = ['user','pharmacy']
