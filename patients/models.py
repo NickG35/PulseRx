960ad9be3536a2a9dad37a79c6f9d45857606550
NickG35 <ngeorge356@gmail.com>
2025-06-05 13:47:49 -0400

    removed redundant fields and made models simpler

--- patients/models.py
+++ patients/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from accounts.models import CustomAccount
-from pharmacy.models import Drug, PharmacyProfile
 
 class PatientProfile(models.Model):
     first_name = models.CharField(max_length=100, null=False)
@@ -9,19 +8,12 @@ class PatientProfile(models.Model):
     dob = models.DateField()
     gender = models.CharField(max_length=10, choices=[('M', 'Male'), ('F', 'Female')])
     phone_number = models.CharField(max_length=20, null=False)
+    pharmacy = models.ForeignKey('pharmacy.PharmacyProfile', on_delete=models.SET_NULL, null=True, related_name='patients')
 
     def __str__(self):
         return f"{self.user}"
 
 class MedicationReminder(models.Model):
-    patient = models.ForeignKey(PatientProfile, on_delete=models.CASCADE)
-    medicine = models.ForeignKey(Drug, on_delete=models.PROTECT)
+    prescription = models.ForeignKey('pharmacy.Prescription', on_delete=models.CASCADE)
     frequency = models.CharField(max_length=100)
     time = models.TimeField()
-
-class PatientPharmacy(models.Model):
-    patient = models.OneToOneField(CustomAccount, on_delete=models.CASCADE, limit_choices_to={'role': 'patient'})
-    pharmacy = models.ForeignKey(PharmacyProfile, on_delete=models.SET_NULL, null=True, related_name='patients')
-
-    def __str__(self):
-        return f"{self.patient.username} → {self.pharmacy}"
