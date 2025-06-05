4ee87340695dbba9df98e4f452432ef64eb0b74c
NickG35 <ngeorge356@gmail.com>
2025-06-05 13:47:05 -0400

    removed patient pharmacy from admin

--- patients/admin.py
+++ patients/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
-from .models import PatientProfile, PatientPharmacy
+from .models import PatientProfile
 
 # Register your models here.
 admin.site.register(PatientProfile)
-admin.site.register(PatientPharmacy)
