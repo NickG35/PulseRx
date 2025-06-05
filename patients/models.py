1fb957f47f369ada82c5874ae5bf428b7aeb6616
NickG35 <ngeorge356@gmail.com>
2025-06-05 14:18:50 -0400

    fixed foreign key for patientprofile and medreminder

--- patients/models.py
+++ patients/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from accounts.models import CustomAccount
+from pharmacy.models import PharmacyProfile, Prescription
 
 class PatientProfile(models.Model):
     first_name = models.CharField(max_length=100, null=False)
@@ -8,12 +9,12 @@ class PatientProfile(models.Model):
     dob = models.DateField()
     gender = models.CharField(max_length=10, choices=[('M', 'Male'), ('F', 'Female')])
     phone_number = models.CharField(max_length=20, null=False)
-    pharmacy = models.ForeignKey('pharmacy.PharmacyProfile', on_delete=models.SET_NULL, null=True, related_name='patients')
+    pharmacy = models.ForeignKey(PharmacyProfile, on_delete=models.SET_NULL, null=True, related_name='patients')
 
     def __str__(self):
         return f"{self.user}"
 
 class MedicationReminder(models.Model):
-    prescription = models.ForeignKey('pharmacy.Prescription', on_delete=models.CASCADE)
+    prescription = models.ForeignKey(Prescription, on_delete=models.CASCADE)
     frequency = models.CharField(max_length=100)
     time = models.TimeField()
