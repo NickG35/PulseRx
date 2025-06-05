2806700008e907f7cda12895258bb427048ed0ba
NickG35 <ngeorge356@gmail.com>
2025-06-05 13:48:11 -0400

    omitted pharmacy form

--- patients/views.py
+++ patients/views.py
@@ -1,6 +1,4 @@
 from django.shortcuts import render, redirect
-from .forms import PharmacistForm
-from .models import PatientPharmacy
 
 def patient_home(request):
     return render(request, 'patient_home.html')
@@ -9,21 +7,7 @@ def prescriptions(request):
     return render(request, 'prescriptions.html')
 
 def my_pharmacy(request):
-    user = request.user
-    patient_profile, _ = PatientPharmacy.objects.get_or_create(patient=user)
-
-    if request.method == 'POST':
-        form = PharmacistForm(request.POST)
-        if form.is_valid():
-            patient_profile.pharmacy = form.cleaned_data['pharmacy']
-            patient_profile.save()
-            return redirect('my_pharmacy')
-        
-    form = PharmacistForm()
-    return render(request, 'my_pharmacy.html' , {
-        'pharmacies': patient_profile.pharmacy,
-        'form': form
-    })
+    return render(request, 'my_pharmacy.html')
 
 def account(request):
     return render(request, 'account.html')
