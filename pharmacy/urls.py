9a7b02dfa9041376861c1f51da68b4f4e5d52ab0
NickG35 <ngeorge356@gmail.com>
2025-06-11 13:45:38 -0400

    implemented patient search url

--- pharmacy/urls.py
+++ pharmacy/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('admin/dashboard', views.pharmacy_home, name='pharmacy_home'),
     path('pharmacist/dashboard', views.pharmacist_home, name='pharmacist_home'),
     path('create_prescriptions', views.create_prescriptions, name='create_prescriptions'),
+    path('patient_search/', views.patient_search, name='patient_search'),
     path('my_patients', views.my_patients, name='my_patients'),
     path('pharmacy_account', views.account, name='pharmacy_account'),
     path('inventory', views.inventory, name='inventory'),
