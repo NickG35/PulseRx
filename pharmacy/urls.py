48eeb4fed80eca33411c4bd45dc67dc3353ba32c
NickG35 <ngeorge356@gmail.com>
2025-06-13 07:31:50 -0400

    implemented patient profile url

--- pharmacy/urls.py
+++ pharmacy/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('patient_search/', views.patient_search, name='patient_search'),
     path('medicine_search/', views.medicine_search, name='medicine_search'),
     path('my_patients', views.my_patients, name='my_patients'),
+    path('patient_profile/<int:patient_id>', views.patient_profile, name='patient_profile'),
     path('pharmacy_account', views.account, name='pharmacy_account'),
     path('inventory', views.inventory, name='inventory'),
     path('drug_detail/<int:drug_id>', views.drug_detail, name='drug_detail'),
