73cd17a6948a4c20c825dce184df77c162039753
NickG35 <ngeorge356@gmail.com>
2025-06-18 13:50:51 -0400

    implemented update account and password forms

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,6 +1,8 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth import get_user_model
+from django.contrib.auth.password_validation import validate_password
+from django.core.exceptions import ValidationError
 from .models import CustomAccount
 
 class UserRegistrationForm(UserCreationForm):
@@ -17,3 +19,52 @@ User = get_user_model()
 class LoginForm (AuthenticationForm):
     username = forms.CharField(max_length=100)
     password = forms.CharField(widget=forms.PasswordInput)
+
+class AccountUpdateForm(forms.ModelForm):
+    class Meta:
+        model = CustomAccount
+        fields = ['username', 'email']
+        widgets = {
+            'username': forms.TextInput(attrs={
+                'class': 'user-info',
+            }),
+            'email': forms.EmailInput(attrs={
+                'class': 'user-info',
+            })
+        }
+        
+
+class PasswordUpdateForm(forms.Form):
+    current_password = forms.CharField(
+        widget=forms.PasswordInput(attrs={
+            'class': 'password-info', 
+            'placeholder': 'Current password',
+        }),
+    )
+    password = forms.CharField(widget=forms.PasswordInput(attrs= {
+            'class': 'password-info',
+            'placeholder': 'new password',
+        }),
+    )
+    confirmation = forms.CharField(widget=forms.PasswordInput(attrs= {
+            'class': 'password-info',
+            'id': 'confirmation',
+            'style': 'display:none;',
+            'placeholder': 'confirm password',
+        }),
+    )
+
+    def clean(self):
+        cleaned_data = super().clean()
+        password = cleaned_data.get("password")
+        confirmation = cleaned_data.get("confirmation")
+        current_password = cleaned_data.get("current_password")
+
+        if password and confirmation and password != confirmation:
+            raise forms.ValidationError("Passwords do not match.")
+        if password and current_password and password == current_password:
+            raise forms.ValidationError("New password must be different than current.")
+        if password:
+            validate_password(password)
+
+        return cleaned_data
