bea00cf6e42db815b76d1953c45ee152165be95e
NickG35 <ngeorge356@gmail.com>
2025-07-07 06:59:38 -0400

    registered message model in accounts to db

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import CustomAccount
+from .models import CustomAccount, Message
 
 # Register your models here.
 admin.site.register(CustomAccount)
+admin.site.register(Message)
