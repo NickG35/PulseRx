09e81fe22205f3c50149a6693f216d3ba0f10b12
NickG35 <ngeorge356@gmail.com>
2025-07-07 07:43:26 -0400

    initiated prescription and my pharmacy views

--- patients/views.py
+++ patients/views.py
@@ -1,13 +1,23 @@
 from django.shortcuts import render, redirect
+from pharmacy.models import Prescription
+from .models import PatientProfile
 
 def patient_home(request):
     return render(request, 'patient_home.html')
 
 def prescriptions(request):
-    return render(request, 'prescriptions.html')
+    patient = PatientProfile.objects.get(user=request.user)
+    all_prescriptions = Prescription.objects.filter(patient=patient).all()
+    return render(request, 'prescriptions.html', {
+        'prescriptions': all_prescriptions,
+    })
 
 def my_pharmacy(request):
-    return render(request, 'my_pharmacy.html')
+    patient = PatientProfile.objects.get(user=request.user)
+    current_pharmacy = patient.pharmacy
+    return render(request, 'my_pharmacy.html', {
+        'pharmacy': current_pharmacy,
+    })
 
 def account(request):
     return render(request, 'account.html')
