8f8d05e3ef4679c83275c18864b503c167961615
NickG35 <ngeorge356@gmail.com>
2025-07-07 07:00:49 -0400

    moved message model to account app

--- accounts/models.py
+++ accounts/models.py
@@ -8,3 +8,14 @@ class CustomAccount(AbstractUser):
         ('patient', 'Patient'),
     )
     role = models.CharField(max_length=20, choices=ROLE_CHOICES)
+
+class Message(models.Model):
+    sender = models.ForeignKey(CustomAccount, related_name='sent_messages', on_delete=models.PROTECT)
+    recipient = models.ForeignKey(CustomAccount, related_name='received_messages', on_delete=models.PROTECT)
+    content = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+    read = models.BooleanField(default=False)
+    read_time = models.DateField(null=True)
+
+    class Meta:
+        db_table = 'pharmacy_message' 
