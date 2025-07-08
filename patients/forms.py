7fb4145d8e0c963848cc856ac33e11883199aa2a
NickG35 <ngeorge356@gmail.com>
2025-07-08 19:04:53 -0400

    initiated reminder form and set prescription field to current user prescription

--- patients/forms.py
+++ patients/forms.py
@@ -1,8 +1,20 @@
 from django import forms
-from .models import PatientProfile
-from pharmacy.models import PharmacyProfile
+from .models import PatientProfile, MedicationReminder
+from pharmacy.models import Prescription
 
 class PatientProfileForm(forms.ModelForm):
     class Meta:
         model = PatientProfile
         exclude = ['user']
+
+class ReminderForm(forms.ModelForm):
+    class Meta:
+        model = MedicationReminder
+        exclude = ['user']
+    
+    def __init__(self, *args, **kwargs):
+        patient = kwargs.pop('patient', None)
+        super().__init__(*args, **kwargs)
+
+        if patient:
+            self.fields['prescription'].queryset = Prescription.objects.filter(patient=patient)
