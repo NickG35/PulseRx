821118464dbaa04e6c59b45d0ef6e28419c3c504
NickG35 <ngeorge356@gmail.com>
2025-07-08 19:07:31 -0400

    returned prescription by name and brand name

--- pharmacy/models.py
+++ pharmacy/models.py
@@ -45,4 +45,7 @@ class Prescription (models.Model):
     prescribed_on = models.DateTimeField(auto_now_add=True)
     expiration_date = models.DateField()
 
+    def __str__(self):
+        return f"{self.medicine.name} ({self.medicine.brand})"
+
 
