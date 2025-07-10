da46d94b4ebe5e977b3f0d0728e60b02a83ef4df
NickG35 <ngeorge356@gmail.com>
2025-07-10 11:25:03 -0400

    registered reminder model to db

--- patients/admin.py
+++ patients/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import PatientProfile
+from .models import PatientProfile, MedicationReminder
 
 # Register your models here.
 admin.site.register(PatientProfile)
+admin.site.register(MedicationReminder)
