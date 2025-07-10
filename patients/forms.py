0cfd7988ddd37b08afb6838a09e58355020b7731
NickG35 <ngeorge356@gmail.com>
2025-07-10 11:24:49 -0400

    removed time input from reminder form

--- patients/forms.py
+++ patients/forms.py
@@ -17,19 +17,9 @@ class ReminderForm(forms.ModelForm):
     class Meta:
         model = MedicationReminder
         exclude = ['user']
-        widgets = {
-            'time': forms.TimeInput(
-                attrs={
-                    'id': 'timeInput'
-                },
-                format='%I:%M %p'
-            )
-        }
     
     def __init__(self, *args, **kwargs):
         patient = kwargs.pop('patient', None)
         super().__init__(*args, **kwargs)
-
-        self.fields['time'].input_formats = ['%I:%M %p']
         if patient:
             self.fields['prescription'].queryset = Prescription.objects.filter(patient=patient)
