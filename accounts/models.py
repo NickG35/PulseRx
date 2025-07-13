e6b0ef2c63e2576c0d92cd75e5eac7f51e323aa5
NickG35 <ngeorge356@gmail.com>
2025-07-13 14:18:25 -0400

    created notification model

--- accounts/models.py
+++ accounts/models.py
@@ -9,6 +9,11 @@ class CustomAccount(AbstractUser):
     )
     role = models.CharField(max_length=20, choices=ROLE_CHOICES)
 
+class Notifications(models.Model):
+    user = models.ForeignKey(CustomAccount, on_delete=models.CASCADE, related_name='notifications')
+    message = models.CharField(max_length=200)
+    time = models.DateTimeField(auto_now_add=True)
+
 class Message(models.Model):
     sender = models.ForeignKey(CustomAccount, related_name='sent_messages', on_delete=models.PROTECT)
     recipient = models.ForeignKey(CustomAccount, related_name='received_messages', on_delete=models.PROTECT)
