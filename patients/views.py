5d28e8c73feb0007d5ad96d1ea81af7dc85e3b9f
NickG35 <ngeorge356@gmail.com>
2025-07-16 13:38:14 -0400

    ensured user submitted times are being created in db and implemented reminder suggestions

--- patients/views.py
+++ patients/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render, redirect
 from pharmacy.models import Prescription
-from .models import PatientProfile, MedicationReminder
+from .models import PatientProfile, MedicationReminder, ReminderTime
 from .forms import ReminderForm, PharmacyForm
+from datetime import datetime
+from django.http import JsonResponse
+import json
 
 def patient_home(request):
     return render(request, 'patient_home.html')
@@ -43,6 +46,14 @@ def reminders(request):
             reminder = form.save(commit=False)
             reminder.user = patient
             reminder.save()
+
+            for time in request.POST:
+                if time.startswith('time_'):
+                    time_str = request.POST[time]
+                    if time_str:
+                        time_obj = datetime.strptime(time_str, "%I:%M %p").time()
+                        ReminderTime.objects.create(reminder=reminder, time=time_obj)
+
             return redirect('reminders')
     else:
         form = ReminderForm(patient=patient)
@@ -54,3 +65,19 @@ def reminders(request):
 
 def messages(request):
     return render(request, 'messages.html')
+
+def reminder_days(request):
+    if request.method == 'POST':
+        try:
+            data = json.loads(request.body)
+            prescription_id = data.get('prescription_id')
+
+            results = Prescription.objects.filter(id=prescription_id)
+            response_data = [{"quantity": obj.quantity, "dosage": obj.medicine.dosage} for obj in results]
+
+            return JsonResponse({"results": response_data})
+        except json.JSONDecodeError:
+            return JsonResponse({"error": "Invalid JSON"}, status=400)
+    else:
+        return JsonResponse({"error": "Only POST requests are allowed"}, status=405)
+
