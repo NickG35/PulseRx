e7592709f5ede31f3418cef381f9d229dacb9960
NickG35 <ngeorge356@gmail.com>
2025-07-16 13:35:27 -0400

    registered reminder time model instance

--- patients/admin.py
+++ patients/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import PatientProfile, MedicationReminder
+from .models import PatientProfile, MedicationReminder, ReminderTime
 
 # Register your models here.
 admin.site.register(PatientProfile)
 admin.site.register(MedicationReminder)
+admin.site.register(ReminderTime)
