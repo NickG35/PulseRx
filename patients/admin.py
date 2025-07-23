e04606c2a293ba77d19bae277c25acc5dfce3e06
NickG35 <ngeorge356@gmail.com>
2025-07-23 15:54:17 -0400

    registered reminder time in medication reminder

--- patients/admin.py
+++ patients/admin.py
@@ -2,6 +2,13 @@ from django.contrib import admin
 from .models import PatientProfile, MedicationReminder, ReminderTime
 
 # Register your models here.
+class ReminderTimeInline(admin.TabularInline):
+    model = ReminderTime
+    extra = 1      # Show 1 empty form by default
+    max_num = 5    # Allow a max of 5 total ReminderTime entries
+
+class MedicationReminderAdmin(admin.ModelAdmin):
+    inlines = [ReminderTimeInline]
+
 admin.site.register(PatientProfile)
-admin.site.register(MedicationReminder)
-admin.site.register(ReminderTime)
+admin.site.register(MedicationReminder, MedicationReminderAdmin)
