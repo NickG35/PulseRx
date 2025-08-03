cc4ff9c5dae8cb2ba982b690987397dea6cb8d58
NickG35 <ngeorge356@gmail.com>
2025-08-03 17:21:59 -0400

    altered notification model to include message and reminder foreign keys

--- accounts/models.py
+++ accounts/models.py
@@ -9,11 +9,6 @@ class CustomAccount(AbstractUser):
     )
     role = models.CharField(max_length=20, choices=ROLE_CHOICES)
 
-class Notifications(models.Model):
-    user = models.ForeignKey(CustomAccount, on_delete=models.CASCADE, related_name='notifications')
-    message = models.CharField(max_length=200)
-    time = models.DateTimeField(auto_now_add=True)
-
 class Message(models.Model):
     sender = models.ForeignKey(CustomAccount, related_name='sent_messages', on_delete=models.PROTECT)
     recipient = models.ForeignKey(CustomAccount, related_name='received_messages', on_delete=models.PROTECT)
@@ -24,3 +19,9 @@ class Message(models.Model):
 
     class Meta:
         db_table = 'pharmacy_message' 
+
+class Notifications(models.Model):
+    user = models.ForeignKey(CustomAccount, on_delete=models.CASCADE, related_name='notifications')
+    message = models.ForeignKey(Message, on_delete=models.CASCADE, related_name='messages', blank=True, null=True)
+    reminder = models.ForeignKey('patients.MedicationReminder', on_delete=models.CASCADE, related_name='reminders', blank=True, null=True)
+    time = models.DateTimeField(auto_now_add=True)
