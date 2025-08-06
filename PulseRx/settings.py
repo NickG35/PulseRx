44ed79cc7b06baf8c0f52bb394801ef71665195f
NickG35 <ngeorge356@gmail.com>
2025-08-06 07:58:04 -0400

    added notification context processor to settings

--- PulseRx/settings.py
+++ PulseRx/settings.py
@@ -65,6 +65,7 @@ TEMPLATES = [
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
+                'accounts.context_processors.notification_display',
                 'django.contrib.messages.context_processors.messages',
             ],
         },
