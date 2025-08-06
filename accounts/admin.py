7cd3fd9a8bcaa6eba5748ed295164e715e0351f9
NickG35 <ngeorge356@gmail.com>
2025-08-06 07:55:00 -0400

    registered notification model to database

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import CustomAccount, Message
+from .models import CustomAccount, Message, Notifications
 
 # Register your models here.
 admin.site.register(CustomAccount)
 admin.site.register(Message)
+admin.site.register(Notifications)
