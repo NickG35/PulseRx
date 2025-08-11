28745ef70d4884098baf08851f17ec57c0b4ec48
NickG35 <ngeorge356@gmail.com>
2025-08-11 05:33:48 -0400

    validation error when day amount is less than 0

--- patients/forms.py
+++ patients/forms.py
@@ -17,7 +17,13 @@ class ReminderForm(forms.ModelForm):
     class Meta:
         model = MedicationReminder
         exclude = ['user', 'start_date','is_active', 'remaining_days', 'restoration_time']
-        
+    
+    def clean_day_amount(self):
+        day_amount = self.cleaned_data.get('day_amount')
+        if day_amount <= 0:
+            raise forms.ValidationError("Day amount must be greater than zero.")
+        return day_amount
+    
     def __init__(self, *args, **kwargs):
         patient = kwargs.pop('patient', None)
         super().__init__(*args, **kwargs)
