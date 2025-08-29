93cc5c56be9d7520d01595d27eba56e4478486f8
NickG35 <ngeorge356@gmail.com>
2025-08-29 07:04:35 -0400

    initiating thread system in model db

--- accounts/models.py
+++ accounts/models.py
@@ -9,16 +9,22 @@ class CustomAccount(AbstractUser):
     )
     role = models.CharField(max_length=20, choices=ROLE_CHOICES)
 
+class Thread(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    last_updated = models.DateTimeField(auto_now_add=True)
+
 class Message(models.Model):
     sender = models.ForeignKey(CustomAccount, related_name='sent_messages', on_delete=models.PROTECT)
     recipient = models.ForeignKey(CustomAccount, related_name='received_messages', on_delete=models.PROTECT)
+    thread = models.ForeignKey(Thread, related_name='thread', on_delete=models.CASCADE)
     content = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True)
     read = models.BooleanField(default=False)
-    read_time = models.DateField(null=True)
+    read_time = models.DateTimeField(null=True, blank=True)
 
     class Meta:
         db_table = 'pharmacy_message' 
+
 
 class Notifications(models.Model):
     user = models.ForeignKey(CustomAccount, on_delete=models.CASCADE, related_name='notifications')
