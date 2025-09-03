719c4a3d3ca054b2d2de7d085d7ee4faf46aece9
NickG35 <ngeorge356@gmail.com>
2025-09-03 10:43:30 -0400

    displaying all pharmacists in pharmacy profile

--- pharmacy/admin.py
+++ pharmacy/admin.py
@@ -1,7 +1,21 @@
 from django.contrib import admin
-from .models import PharmacyProfile, Prescription, Drug
+from .models import PharmacyProfile, PharmacistProfile, Prescription, Drug
 
 # Register your models here.
-admin.site.register(PharmacyProfile)
 admin.site.register(Prescription)
 admin.site.register(Drug)
+
+
+class PharmacistInline(admin.TabularInline):  # or admin.StackedInline for a bigger form
+    model = PharmacistProfile
+    extra = 1  # how many empty forms to show by default
+    fields = ('first_name', 'last_name', 'user')
+
+@admin.register(PharmacyProfile)
+class PharmacyProfileAdmin(admin.ModelAdmin):
+    list_display = ('pharmacy_name', 'city', 'state')
+    inlines = [PharmacistInline]
+
+@admin.register(PharmacistProfile)
+class PharmacistProfileAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'pharmacy')
