1b9ca71adfda11dd9c0f8aee158433428169a15e
NickG35 <ngeorge356@gmail.com>
2025-09-03 10:28:40 -0400

    registered thread model to admin database

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import CustomAccount, Message, Notifications
+from .models import CustomAccount, Message, Notifications, Thread
 
 # Register your models here.
 admin.site.register(CustomAccount)
 admin.site.register(Message)
+admin.site.register(Thread)
 admin.site.register(Notifications)
