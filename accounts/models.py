36c9905359afe8de90eb3c079467cf54baed21aa
NickG35 <ngeorge356@gmail.com>
2025-09-03 10:39:06 -0400

    adjusted thread and message model configuration

--- accounts/models.py
+++ accounts/models.py
@@ -10,13 +10,17 @@ class CustomAccount(AbstractUser):
     role = models.CharField(max_length=20, choices=ROLE_CHOICES)
 
 class Thread(models.Model):
+    participant = models.ManyToManyField(CustomAccount)
     created_at = models.DateTimeField(auto_now_add=True)
-    last_updated = models.DateTimeField(auto_now_add=True)
+    last_updated = models.DateTimeField(auto_now=True)
+
+    def latest_message(self):
+        return self.messages.last()
 
 class Message(models.Model):
     sender = models.ForeignKey(CustomAccount, related_name='sent_messages', on_delete=models.PROTECT)
     recipient = models.ForeignKey(CustomAccount, related_name='received_messages', on_delete=models.PROTECT)
-    thread = models.ForeignKey(Thread, related_name='thread', on_delete=models.CASCADE)
+    thread = models.ForeignKey(Thread, related_name='messages', on_delete=models.CASCADE)
     content = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True)
     read = models.BooleanField(default=False)
