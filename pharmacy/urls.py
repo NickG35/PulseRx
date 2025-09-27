224b6319b73627aa5af338c44baa5c4cf67b827f
NickG35 <ngeorge356@gmail.com>
2025-09-27 13:16:34 -0400

    added both resupply and contact admin urls

--- pharmacy/urls.py
+++ pharmacy/urls.py
@@ -10,5 +10,7 @@ urlpatterns = [
     path('my_patients', views.my_patients, name='my_patients'),
     path('patient_profile/<int:patient_id>', views.patient_profile, name='patient_profile'),
     path('inventory', views.inventory, name='inventory'),
+    path('resupply/<int:drug_id>', views.resupply, name='resupply'),
+    path('contact_admin/<int:drug_id>', views.contact_admin, name='contact_admin'),
     path('drug_detail/<int:drug_id>', views.drug_detail, name='drug_detail'),
 ]
