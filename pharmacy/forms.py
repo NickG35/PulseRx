96c622abf6c4c570b865ab54411b36e57f9dd137
NickG35 <ngeorge356@gmail.com>
2025-10-16 13:27:34 -0400

    excluded new refill pending field from prescription form to set manually

--- pharmacy/forms.py
+++ pharmacy/forms.py
@@ -24,7 +24,7 @@ class PrescriptionForm(forms.ModelForm):
 
         class Meta:
             model = Prescription
-            exclude = ['prescribed_by', 'refills_left', 'patient', 'medicine']
+            exclude = ['prescribed_by', 'refills_left', 'refill_pending', 'patient', 'medicine']
         
         
         def __init__(self, *args, **kwargs):
