a173d8a1901b2a536ee13856caea72a4d9275d02
NickG35 <ngeorge356@gmail.com>
2025-10-30 12:37:20 -0400

    added messages path to routing

--- accounts/routing.py
+++ accounts/routing.py
@@ -3,4 +3,5 @@ from . import consumers
 
 websocket_urlpatterns = [
     re_path(r'ws/notifications/$', consumers.NotificationConsumer.as_asgi()),
+    re_path(r'ws/messages/(?P<thread_id>\d+)/$', consumers.MessageConsumer.as_asgi()),
 ]
