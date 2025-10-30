46d29fbac182eba785c3fc273313d3a3d84d3640
NickG35 <ngeorge356@gmail.com>
2025-10-30 12:38:07 -0400

    removed send messages path due to removal of ajax function

--- accounts/urls.py
+++ accounts/urls.py
@@ -9,7 +9,6 @@ urlpatterns = [
     path('account_settings', views.account_settings, name='account_settings'),
     path('messages', views.account_messages, name='messages'),
     path('thread/<int:thread_id>', views.thread_view, name='threads'),
-    path('send_messages', views.send_messages, name='send_messages'),
     path('message_search', views.message_search, name='message_search'),
     path('patient_thread', views.patient_thread, name='patient_thread'),
     path('delete_notification', views.delete_notification, name='delete_notification'),
