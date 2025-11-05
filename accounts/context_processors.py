efc35d0385630f4cc6591550df6128a3bc51ca6f
NickG35 <ngeorge356@gmail.com>
2025-11-05 18:04:06 -0500

    added unread count to display on badge on notification bell

--- accounts/context_processors.py
+++ accounts/context_processors.py
@@ -3,5 +3,9 @@ from .models import Notifications
 def notification_display(request):
     if request.user.is_authenticated:
         notifications = Notifications.objects.filter(user=request.user).order_by('-time')
-        return {'notifications': notifications}
+        unread_count = notifications.filter(is_read=False).count()
+        return {
+            'notifications': notifications,
+            'unread_count': unread_count
+        }
     return {}
