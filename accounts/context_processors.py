0fe72e2b7a932def32c680ebee02867e65a2c761
NickG35 <ngeorge356@gmail.com>
2025-11-06 20:02:19 -0500

    added unread messages to display

--- accounts/context_processors.py
+++ accounts/context_processors.py
@@ -1,11 +1,21 @@
-from .models import Notifications
+from .models import Notifications, Message
+from pharmacy.models import PharmacistProfile
 
 def notification_display(request):
     if request.user.is_authenticated:
         notifications = Notifications.objects.filter(user=request.user).order_by('-time')
         unread_count = notifications.filter(is_read=False).count()
+        if request.user.role == 'pharmacist':
+            pharmacist = PharmacistProfile.objects.get(user=request.user)
+            pharmacy = pharmacist.pharmacy
+            unread_messages = Message.objects.filter(recipient=pharmacy.user, read=False).exclude(sender__role='system').count()
+        else:
+            unread_messages = Message.objects.filter(recipient=request.user, read=False).exclude(sender__role='system').count()
+
+        print(f"User: {request.user} | Role: {request.user.role} | Unread messages: {unread_messages}")
         return {
             'notifications': notifications,
-            'unread_count': unread_count
+            'unread_count': unread_count,
+            'unread_messages': unread_messages
         }
     return {}
