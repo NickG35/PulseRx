7d0e9ad0c751f4ff22defdf8ea399c1c9f7e6614
NickG35 <ngeorge356@gmail.com>
2025-11-18 05:54:10 -0500

    deleted read field in message model and implemented read status model

--- accounts/context_processors.py
+++ accounts/context_processors.py
@@ -1,16 +1,29 @@
-from .models import Notifications, Message
+from .models import Notifications, ReadStatus
 from pharmacy.models import PharmacistProfile
 
 def notification_display(request):
     if request.user.is_authenticated:
         notifications = Notifications.objects.filter(user=request.user).order_by('-time')
         unread_count = notifications.filter(is_read=False).count()
+
         if request.user.role == 'pharmacist':
             pharmacist = PharmacistProfile.objects.get(user=request.user)
             pharmacy = pharmacist.pharmacy
-            unread_messages = Message.objects.filter(recipient=pharmacy.user, read=False).exclude(sender__role='system').count()
+
+            unread_messages = ReadStatus.objects.filter(
+                user=pharmacy.user,
+                read=False
+            ).exclude(
+                message__sender__role='system'
+            ).count()
+
         else:
-            unread_messages = Message.objects.filter(recipient=request.user, read=False).exclude(sender__role='system').count()
+            unread_messages = ReadStatus.objects.filter(
+                user=request.user,
+                read=False
+            ).exclude(
+                message__sender__role='system'
+            ).count()
 
         print(f"User: {request.user} | Role: {request.user.role} | Unread messages: {unread_messages}")
         return {
@@ -18,4 +31,5 @@ def notification_display(request):
             'unread_count': unread_count,
             'unread_messages': unread_messages
         }
+
     return {}
