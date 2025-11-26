af79b6d62a85f231765883c69f5cb09e29f9f68d
NickG35 <ngeorge356@gmail.com>
2025-11-26 16:44:19 -0500

    combined all user noti counts instead of specifying pharmacists

--- accounts/context_processors.py
+++ accounts/context_processors.py
@@ -1,31 +1,18 @@
 from .models import Notifications, ReadStatus
-from pharmacy.models import PharmacistProfile
 
 def notification_display(request):
     if request.user.is_authenticated:
         notifications = Notifications.objects.filter(user=request.user).order_by('-time')
         unread_count = notifications.filter(is_read=False).count()
 
-        if request.user.role == 'pharmacist':
-            pharmacist = PharmacistProfile.objects.get(user=request.user)
-            pharmacy = pharmacist.pharmacy
+     
+        unread_messages = ReadStatus.objects.filter(
+            user=request.user,
+            read=False
+        ).exclude(
+            message__sender__role='system'
+        ).count()
 
-            unread_messages = ReadStatus.objects.filter(
-                user=pharmacy.user,
-                read=False
-            ).exclude(
-                message__sender__role='system'
-            ).count()
-
-        else:
-            unread_messages = ReadStatus.objects.filter(
-                user=request.user,
-                read=False
-            ).exclude(
-                message__sender__role='system'
-            ).count()
-
-        print(f"User: {request.user} | Role: {request.user.role} | Unread messages: {unread_messages}")
         return {
             'notifications': notifications,
             'unread_count': unread_count,
