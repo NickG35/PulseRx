1f5dc1232b352a2d68cac231396c5cc438baa0f7
NickG35 <ngeorge356@gmail.com>
2025-12-10 14:35:10 -0500

    Add age validation and make pharmacy optional in patient registration

--- patients/forms.py
+++ patients/forms.py
@@ -2,11 +2,53 @@ from django import forms
 from .models import PatientProfile, MedicationReminder
 from pharmacy.models import PharmacyProfile
 from pharmacy.models import Prescription
+from datetime import date
 
 class PatientProfileForm(forms.ModelForm):
     class Meta:
         model = PatientProfile
         exclude = ['user']
+        widgets = {
+            'dob': forms.DateInput(attrs={
+                'type': 'date',
+                'max': '9999-12-31',  # Prevent future dates in browser
+            }),
+            'gender': forms.Select(attrs={
+                'class': 'select select-bordered w-full',
+            }),
+            'pharmacy': forms.Select(attrs={
+                'class': 'select select-bordered w-full',
+            }),
+            'first_name': forms.TextInput(attrs={
+                'placeholder': 'First Name',
+            }),
+            'last_name': forms.TextInput(attrs={
+                'placeholder': 'Last Name',
+            }),
+        }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # Make pharmacy optional
+        self.fields['pharmacy'].required = False
+
+    def clean_dob(self):
+        dob = self.cleaned_data.get('dob')
+        if dob:
+            today = date.today()
+            if dob > today:
+                raise forms.ValidationError("Date of birth cannot be in the future.")
+
+            # Calculate age
+            age = today.year - dob.year - ((today.month, today.day) < (dob.month, dob.day))
+
+            if age < 13:
+                raise forms.ValidationError("You must be at least 13 years old to register.")
+
+            if dob.year < 1900:
+                raise forms.ValidationError("Please enter a valid date of birth.")
+
+        return dob
 
 class PharmacyForm(forms.ModelForm):
     class Meta:
